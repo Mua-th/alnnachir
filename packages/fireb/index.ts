4cf568d929dfc8d6cd54b1db6a664c0ce826b938
mua-th <mouadelgharbi2002@gmail.com>
2024-10-19 00:23:55 +0200

    adding .env

--- packages/fireb/index.ts
+++ packages/fireb/index.ts
@@ -8,16 +8,20 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBLL3RdvoZC5XRuXgj5IKcFJTggNHTG1V0",
-  authDomain: "alnnachir.firebaseapp.com",
-  projectId: "alnnachir",
-  storageBucket: "alnnachir.appspot.com",
-  messagingSenderId: "680902582981",
-  appId: "1:680902582981:web:55d656cf5f1f44c5b1c6ac",
-  measurementId: "G-JGS4SKCQLV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+
+if (typeof window !== "undefined") {
+  const analytics = getAnalytics(app);
+}
+
