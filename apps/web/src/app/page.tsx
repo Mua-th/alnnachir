f6c6a99ae05cb04275b619ccf63e931ce84acf61
mua-th <mouadelgharbi2002@gmail.com>
2024-10-19 01:07:39 +0200

    fixing layout in home

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,80 +6,60 @@ import Article from '@/components/article';
 
 export default function Page(): JSX.Element {
   return (
-    <main dir='rtl' className="grid grid-rows-2 justify-normal items-center  min-h-screen w-full">
-      <div className="items-start flex  justify-center h-full w-full  ">
-      <div className=" font-lateef text-5xl font-normal text-black w-full md:w-2/5 text-right  h-5/6" >
-        
-      <TypeAnimation
-        sequence={[
-          // Initial phrase
-          'الناشر :',
-          1000,
-          // Building up phrases
-          'الناشر :\nمقالات',
-          500,
-          'الناشر :\nمقالات | آراء',
-          500,
-          'الناشر :\nمقالات | آراء | تدوينات',
-          1000,
-          // Full set of phrases
-          'الناشر :\nمقالات | آراء | تدوينات\nتأملات | خواطر | أفكار',
-          1500,
-         
-          'الناشر :\nرحلة في عالم المعرفة',
-          1000,
-          // Finale
-          'الناشر :\nنافذة على عالم الإبداع',
-          2000,
-          // Reset
-          '',
-          1000,
-        ]}
-        wrapper="div"
-        speed={10}
-        deletionSpeed={20}
-        preRenderFirstString={true}
-        style={{
-          direction: 'rtl',
-          display: 'inline-block',
-          whiteSpace: 'pre-line',
-          textAlign: 'right',
-        }}
-        repeat={Infinity}
-      />
-        
-        
-        
-        
-      </div>
-      </div>
-      <div className='items-start flex flex-col justify-start h-full '>
-      <Separator></Separator>
-      <div className='flex justify-between w-full '>
-        <div className='flex flex-col gap-5'>
-        <div className=''>
-        <Article></Article>
-
-          </div>
-        <div className=''>
-        <Article></Article>
-
-          </div>
-
-        <div className=''>
-          <Article></Article>
-          </div>
-   
+    <main dir='rtl' className="grid grid-rows-[2fr_3fr] grid-flow-row-dense items-start min-h-screen w-full">
+      <div className="items-start flex justify-self-end justify-center w-full overflow-hidden">
+        <div className="font-lateef text-5xl font-normal text-black w-full md:w-2/5 text-right">
+          <TypeAnimation
+            sequence={[
+              // Initial phrase
+              'الناشر :',
+              1000,
+              // Building up phrases
+              'الناشر :\nمقالات',
+              500,
+              'الناشر :\nمقالات | آراء',
+              500,
+              'الناشر :\nمقالات | آراء | تدوينات',
+              1000,
+              // Full set of phrases
+              'الناشر :\nمقالات | آراء | تدوينات\nتأملات | خواطر | أفكار',
+              1500,
+              'الناشر :\nرحلة في عالم المعرفة',
+              1000,
+              // Finale
+              'الناشر :\nنافذة على عالم الإبداع',
+              2000,
+              // Reset
+              '',
+              1000,
+            ]}
+            wrapper="div"
+            speed={10}
+            deletionSpeed={20}
+            preRenderFirstString={true}
+            style={{
+              direction: 'rtl',
+              display: 'inline-block',
+              whiteSpace: 'pre-line',
+              textAlign: 'right',
+            }}
+            repeat={Infinity}
+          />
         </div>
-        <div>
-          wawa
+      </div>
+      <div className='flex flex-col justify-start overflow-auto'>
+        <Separator className="mb-4" />
+        <div className='flex justify-between w-full'>
+          <div className='flex flex-col gap-5 flex-grow'>
+            <Article />
+            <Article />
+            <Article />
+          </div>
+          <div className='w-1/4'>
+            wawa
+          </div>
         </div>
-   
-      
       </div>
-      </div>
-    
-    
     </main>
   );
 }
